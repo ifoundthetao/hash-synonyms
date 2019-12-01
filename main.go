e193bcdd9effa7cc1fd292383294c5a523771a52
Timothy Bolton <bolton.timothy@gmail.com>
2019-12-01 16:54:58 -0600

    Add more robust error checking.

--- main.go
+++ main.go
@@ -6,25 +6,43 @@ import (
     "io/ioutil"
     "os"
 )
+const NO_ORIGINAL_INPUT_FILE_ERROR_NUMBER int = 1
+const NO_MODDED_FILE_ERROR_NUMBER int = 2
 
-func checkForErrors(e error) {
-    if e != nil {
-        fmt.Println("No File Given")
-        os.Exit(1)
-    }
+func getErrorMessages() []string {
+    errorMessages := make([]string, 3)
+    errorMessages[NO_ORIGINAL_INPUT_FILE_ERROR_NUMBER] = "No original input file provided"
+    errorMessages[NO_MODDED_FILE_ERROR_NUMBER] = "No modified file provided"
+
+    return errorMessages
 }
 
 func validateStartOptions() (*string, *string) {
+    errorMessages := getErrorMessages()
     inputFileLocation := ""
     moddedFileLocation := ""
 
-    flag.StringVar(&inputFileLocation, "include-file", "Original input file", "")
-    flag.StringVar(&inputFileLocation, "i", "Original input file", inputFileLocation)
+    flag.StringVar(&inputFileLocation, "include-file", "", "Original input file")
+    flag.StringVar(&inputFileLocation, "i", inputFileLocation, "Original input file")
 
-    flag.StringVar(&moddedFileLocation, "modified-file", "Modified file", "")
-    flag.StringVar(&moddedFileLocation, "m", "Modded file", moddedFileLocation)
+    flag.StringVar(&moddedFileLocation, "modified-file", "", "Modified file")
+    flag.StringVar(&moddedFileLocation, "m", moddedFileLocation, "Modified file")
 
     flag.Parse()
+
+    errorNumber := 0
+
+    switch {
+    case inputFileLocation == "":
+        errorNumber = NO_ORIGINAL_INPUT_FILE_ERROR_NUMBER
+    case moddedFileLocation == "":
+        errorNumber = NO_MODDED_FILE_ERROR_NUMBER
+    }
+    hasError := errorNumber > 0
+    if hasError {
+        fmt.Println("Error:", errorMessages[errorNumber])
+        os.Exit(errorNumber)
+    }
 
     return &inputFileLocation, &moddedFileLocation
 }
@@ -37,7 +55,6 @@ func main() {
     fmt.Println(*moddedFileLocationPtr)
 
     fileContents, err := ioutil.ReadFile(*inputFileLocationPtr)
-    checkForErrors(err)
 
     md5Hasher := md5.New()
 
