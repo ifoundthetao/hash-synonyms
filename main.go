446ed8d4633b52f3691456aee947776bd4b643f1
Timothy Bolton <bolton.timothy@gmail.com>
2019-12-01 15:10:17 -0600

    hash from hardcoded file location

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 import (
     "crypto/md5"
     "fmt"
+    "io/ioutil"
 )
 
+func checkForErrors(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
 func main() {
-    stringToHash := "foo bar this is hashed"
+    fileContents, err := ioutil.ReadFile("./file.txt")
+    checkForErrors(err)
 
     md5Hasher := md5.New()
 
-    md5Hasher.Write([]byte(stringToHash))
+    md5Hasher.Write([]byte(fileContents))
     md5Hash := md5Hasher.Sum(nil)
 
     fmt.Printf("%x\n", md5Hash)
